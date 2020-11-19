641281a5153ba8a712b46c6495642946b7f595d8
DaNoobBoii <69998540+DaNoobBoii@users.noreply.github.com>
2020-11-19 13:52:38 +0700

    Re-New Tamplate

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const nav = document.querySelector('.navLinks');
-const links = document.querySelectorAll('#navLink');
 const burger = document.querySelector('.burger');
-const navbar = document.querySelector('nav');
+let navbar = document.querySelector('nav');
+var myVar;
 
 console.log(navbar);
 
-$(document).ready(function () {
+$(document).ready(function() {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 200) {
-            $(navbar).css('backgroundColor','rgb(35,35,35)');
+        if($(window).scrollTop() > 10) {
+            $(navbar).css('backgroundColor','#eee');
+        } else {
+            $(navbar).css('backgroundColor','rgba(255,255,255,.75)');
         }
-        else {
-            $(navbar).css('backgroundColor','rgba(35,35,35,0)')
-        }
-    });
+    })
+})
+
+function change(x) {
+    x.classList.toggle('change');
+}
+
+burger.addEventListener('click', function() {
+    nav.classList.toggle('active');
 });
 
 new Typed('#typed',{
-    strings : ["Handsome_Hanz (Discord)","DaNoobBoii (Github)", "Shogun Commander (Youtube)"],
-    typeSpeed : 60,
-    delaySpeed : 200,
-    loop : false
+    strings : ["My Name Is Handsome_Hanz","I am Front-End Web Developer","I Can't Use Javascript yet","I Have Speciality In CSS"],
+    typeSpeed : 90,
+    delaySpeed : 1000,
+    loop : true
 });
 
 (function () {
@@ -34,20 +41,10 @@ new Typed('#typed',{
 
         lastScrollTop = currentScrollTop;
     });
-})();
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
+}) ();
 
 $("a[href='#top']").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
 });
 
-
-function change(x) {
-    x.classList.toggle('change');
-}
-
-AOS.init();
---
+++ jQueryApp.js
@@ -0,0 +1,5 @@
+const nav = document.querySelector('.navLinks');
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('nav');
+
+
